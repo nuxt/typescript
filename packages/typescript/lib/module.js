67fc1d2605ee4ad8cbcec76d9703f3eb7f84a6b8
Kevin Marrec <kevin@marrec.io>
2019-05-29 15:21:57 +0200

    feat: initial module implementation

--- packages/typescript/lib/module.js
+++ packages/typescript/lib/module.js
@@ -1,23 +1,70 @@
-module.exports = function () {
-  // TODO: Migrate from packages/webpack/src/config/base.js
-  // if (_typescript.build && buildOptions.typescript && buildOptions.typescript.ignoreNotFoundWarnings) {
-  //   filters.push(
-  //     warn => warn.name === 'ModuleDependencyWarning' &&
-  //       /export .* was not found in /.test(warn.message)
-  //   )
-  // }
+const path = require('path')
+const consola = require('consola')
 
-  // TODO: Migrate from packages/webpack/src/config/client.js
-  // TypeScript type checker
-  // Only performs once per client compilation and only if `ts-loader` checker is not used (transpileOnly: true)
-  // if (_typescript.build && buildOptions.typescript && buildOptions.typescript.typeCheck && !this.isModern && this.loaders.ts.transpileOnly) {
-  //   const ForkTsCheckerWebpackPlugin = require(this.buildContext.nuxt.resolver.resolveModule('fork-ts-checker-webpack-plugin'))
-  //   plugins.push(new ForkTsCheckerWebpackPlugin(Object.assign({
-  //     vue: true,
-  //     tsconfig: path.resolve(rootDir, 'tsconfig.json'),
-  //     tslint: false, // We recommend using ESLint so we set this option to `false` by default
-  //     formatter: 'codeframe',
-  //     logger: consola
-  //   }, buildOptions.typescript.typeCheck)))
-  // }
+const defaults = {
+  typeCheck: true,
+  ignoreNotFoundWarnings: true
 }
+
+function tsModule(_moduleOptions) {
+  // Combine options
+  const moduleOptions = Object.assign(
+    defaults,
+    this.options.typescript,
+    _moduleOptions
+  )
+
+  this.options.extensions.push('ts')
+
+  /*
+    TODO: Extend Builder supportedExtensions to watch .ts/.tsx files | https://github.com/nuxt/nuxt.js/blob/dev/packages/builder/src/builder.js#L47
+    HOW: Refactor core so it can be extended by modules
+  */
+
+  /*
+    TODO: Extend createRoutes supportedExtensions to handle .ts/.tsx files as routes | https://github.com/nuxt/nuxt.js/blob/dev/packages/utils/src/route.js#L135
+    HOW: Refactor core so it can be extended by modules
+  */
+
+  this.extendBuild((config, { isClient, isModern }) => {
+    config.resolve.extensions.push('.ts', '.tsx')
+
+    config.module.rules.push(...['ts', 'tsx'].map(ext =>
+      ({
+        test: new RegExp(`\\.${ext}$`, 'i'),
+        use: [
+          /*
+            TODO: Add babel-loader here | https://github.com/nuxt/nuxt.js/blob/dev/packages/webpack/src/config/base.js#L217
+            HOW: Find a way get babelLoader object (already filled with correct babel options)
+          */
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              [`append${ext.charAt(0).toUpperCase() + ext.slice(1)}SuffixTo`]: [/\.vue$/]
+            }
+          }
+        ]
+      })
+    ))
+
+    if (moduleOptions.typeCheck && isClient && !isModern) {
+      const ForkTsCheckerWebpackPlugin = require(this.nuxt.resolver.resolveModule('fork-ts-checker-webpack-plugin'))
+      config.plugins.push(new ForkTsCheckerWebpackPlugin(Object.assign({
+        vue: true,
+        tsconfig: path.resolve(this.options.rootDir, 'tsconfig.json'),
+        tslint: false, // We recommend using ESLint so we set this option to `false` by default
+        formatter: 'codeframe',
+        logger: consola
+      }, moduleOptions.typeCheck)))
+    }
+
+    /*
+      TODO: ignoreNotFoundWarnings | https://github.com/nuxt/nuxt.js/blob/dev/packages/webpack/src/config/base.js#L432
+      HOW: Refactor core so we can extend filters of the WarnFixPlugin
+    */
+  })
+}
+
+module.exports = tsModule
+module.exports.meta = require('../package.json')
