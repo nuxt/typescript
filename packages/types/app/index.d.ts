dd453b44e2a21b6f4377dfd9d5e7df1b5e918df4
Kevin Marrec <kevin@marrec.io>
2019-06-25 11:53:44 +0200

    feat(types): reflect nuxt#5946 & nuxt#5950 changes

--- packages/types/app/index.d.ts
+++ packages/types/app/index.d.ts
@@ -6,6 +6,9 @@ import { Store } from 'vuex'
 // augment typings of Vue.js
 import './vue'
 
+// augment typings of Vuex
+import './vuex'
+
 type Dictionary<T> = { [key: string]: T }
 
 type NuxtState = Dictionary<any>
@@ -36,7 +39,7 @@ export interface Context {
   res: ServerResponse
   redirect(status: number, path: string, query?: Route['query']): void
   redirect(path: string, query?: Route['query']): void
-  error(params: ErrorParams): void
+  error(params: NuxtError): void
   nuxtState: NuxtState
   beforeNuxtRender(fn: (params: { Components: VueRouter['getMatchedComponents'], nuxtState: NuxtState }) => void): void
 }
@@ -65,9 +68,10 @@ export interface Transition {
   leaveCancelled?(el: HTMLElement): void
 }
 
-export interface ErrorParams {
-  statusCode?: number
+export interface NuxtError {
   message?: string
+  path?: string
+  statusCode?: number
 }
 
 export interface NuxtLoading extends Vue {
