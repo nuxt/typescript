62e8f56b213b541887420b3cee898d868e681c23
Kevin Marrec <kevin@marrec.io>
2019-07-05 15:10:34 +0200

    chore: remove experimentalDecorators & noImplicitAny rules

---
+++ packages/typescript-build/test/fixture/shims-tsx.d.ts
@@ -0,0 +1,12 @@
+// eslint-disable-next-line
+import Vue, { VNode } from 'vue'
+
+declare global {
+  namespace JSX {
+    interface Element extends VNode {}
+    interface ElementClass extends Vue {}
+    interface IntrinsicElements {
+      [elem: string]: any
+    }
+  }
+}
