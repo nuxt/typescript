b5232c7746a5c0696faea2b95e74d322e65d6669
Kevin Marrec <kevin@marrec.io>
2019-08-09 21:15:09 +0200

    chore: remove eslint package

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,12 @@
 module.exports = {
   extends: [
-    './packages/typescript-eslint-config'
-  ]
+    '@nuxtjs'
+  ],
+  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'error'
+  }
 }
