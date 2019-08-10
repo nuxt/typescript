f878084697112dd132bd74761f509ff3f745198d
Kevin Marrec <kevin@marrec.io>
2019-08-10 21:18:40 +0200

    chore: use @nuxtjs/eslint-config-typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,5 @@
 module.exports = {
   extends: [
-    '@nuxtjs'
-  ],
-  plugins: ['@typescript-eslint'],
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
-  },
-  rules: {
-    '@typescript-eslint/no-unused-vars': ['error', { args: 'all', argsIgnorePattern: '^_' }]
-  }
+    '@nuxtjs/eslint-config-typescript'
+  ]
 }
