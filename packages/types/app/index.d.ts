255383dbf5999a65c2fd115a43b51cac0c958c44
Kevin Marrec <kevin@marrec.io>
2019-08-22 14:47:10 +0200

    fix(types): missing type for this.$nuxt.error

--- packages/types/app/index.d.ts
+++ packages/types/app/index.d.ts
@@ -91,6 +91,7 @@ export interface NuxtAppOptions extends ComponentOptions<Vue> {
 export interface NuxtApp extends Vue {
   $options: NuxtAppOptions
   $loading: NuxtLoading
+  error(params: NuxtError): void
   isOffline: boolean
   isOnline: boolean
 }
