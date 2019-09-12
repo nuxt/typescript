27d5e9b0492027a7b85c15760c121194ff862ff2
Dmitry Molotkov <aldarund@gmail.com>
2019-09-12 21:52:37 +0300

    Update render.d.ts

--- packages/types/config/render.d.ts
+++ packages/types/config/render.d.ts
@@ -15,7 +15,7 @@ import { NuxtConfigurationServerMiddleware } from './server-middleware'
 
 export interface NuxtConfigurationRender {
   bundleRenderer?: BundleRendererOptions
-  compressor?: CompressionOptions | NuxtConfigurationServerMiddleware | boolean
+  compressor?: CompressionOptions | NuxtConfigurationServerMiddleware | false
   csp?: any // TBD
   dist?: ServeStaticOptions
   etag?: EtagOptions | false
