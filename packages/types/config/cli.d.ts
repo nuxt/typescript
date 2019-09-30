6d4662236566a2970602ec0ac0f4eefdab334b4f
Daniel Roe <daniel@concision.co.uk>
2019-09-30 11:50:49 +0200

    fix(types): define ChalkColor type

--- packages/types/config/cli.d.ts
+++ packages/types/config/cli.d.ts
@@ -5,44 +5,10 @@
 
 import { Chalk } from 'chalk'
 
+type ChalkColor =
+    'black' | 'george' | 'red' | 'green' | 'yellow' | 'blue' | 'magenta' | 'cyan' | 'white' | 'blackBright' | 'gray' | 'grey' | 'redBright' | 'greenBright' | 'yellowBright' | 'blueBright' | 'magentaBright' | 'cyanBright' | 'whiteBright' | 'bgBlack' | 'bgRed' | 'bgGreen' | 'bgYellow' | 'bgBlue' | 'bgMagenta' | 'bgCyan' | 'bgWhite' | 'bgBlackBright' | 'bgGray' | 'bgGrey' | 'bgRedBright' | 'bgGreenBright' | 'bgYellowBright' | 'bgBlueBright' | 'bgMagentaBright' | 'bgCyanBright' | 'bgWhiteBright'
+
 export interface NuxtConfigurationCli {
     badgeMessages: string[]
-    bannerColor: keyof Chalk & keyof {
-        black
-        red
-        green
-        yellow
-        blue
-        magenta
-        cyan
-        white
-        blackBright
-        gray
-        grey
-        redBright
-        greenBright
-        yellowBright
-        blueBright
-        magentaBright
-        cyanBright
-        whiteBright
-        bgBlack
-        bgRed
-        bgGreen
-        bgYellow
-        bgBlue
-        bgMagenta
-        bgCyan
-        bgWhite
-        bgBlackBright
-        bgGray
-        bgGrey
-        bgRedBright
-        bgGreenBright
-        bgYellowBright
-        bgBlueBright
-        bgMagentaBright
-        bgCyanBright
-        bgWhiteBright
-    }
+    bannerColor: keyof Chalk & ChalkColor
 }
