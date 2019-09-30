d207b723f4c1e8def28ffd206de934bbe3c86877
Daniel Roe <daniel@concision.co.uk>
2019-09-30 11:37:29 +0200

    feat(types): add cli types

---
+++ packages/types/config/cli.d.ts
@@ -0,0 +1,48 @@
+/**
+ * NuxtConfigurationCli
+ * Documentation: https://nuxtjs.org/api/configuration-cli
+ */
+
+import { Chalk } from 'chalk'
+
+export interface NuxtConfigurationCli {
+    badgeMessages: string[]
+    bannerColor: keyof Chalk & keyof {
+        black
+        red
+        green
+        yellow
+        blue
+        magenta
+        cyan
+        white
+        blackBright
+        gray
+        grey
+        redBright
+        greenBright
+        yellowBright
+        blueBright
+        magentaBright
+        cyanBright
+        whiteBright
+        bgBlack
+        bgRed
+        bgGreen
+        bgYellow
+        bgBlue
+        bgMagenta
+        bgCyan
+        bgWhite
+        bgBlackBright
+        bgGray
+        bgGrey
+        bgRedBright
+        bgGreenBright
+        bgYellowBright
+        bgBlueBright
+        bgMagentaBright
+        bgCyanBright
+        bgWhiteBright
+    }
+}
