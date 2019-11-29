77b97564e48aa632d117e71f626b126e148aa883
Daniel Roe <daniel@concision.co.uk>
2019-11-29 12:13:55 +0000

    fix: add type for allowedSources

--- packages/types/config/render.d.ts
+++ packages/types/config/render.d.ts
@@ -31,7 +31,7 @@ interface ServePlaceholderOptions {
 type CspPolicyName = 'child-src' | 'connect-src' | 'default-src' | 'font-src' | 'frame-src' | 'img-src' | 'manifest-src' | 'media-src' | 'object-src' | 'prefetch-src' | 'script-src' | 'script-src-elem' | 'script-src-attr' | 'style-src' | 'style-src-elem' | 'style-src-attr' | 'worker-src' | 'base-uri' | 'plugin-types' | 'sandbox' | 'form-action' | 'frame-ancestors' | 'navigate-to' | 'report-uri' | 'report-to' | 'block-all-mixed-content' | 'referrer' | 'require-sri-for' | 'trusted-types' | 'upgrade-insecure-requests'
 interface CspOptions {
   addMeta?: boolean
-  allowedSources?: {}
+  allowedSources?: string[]
   hashAlgorithm?: string
   policies?: Record<CspPolicyName, string[]>
   reportOnly?: boolean
