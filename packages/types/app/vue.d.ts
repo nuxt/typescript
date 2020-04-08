c4b548fc0a95e1515a35ee904946a4b106ba61e4
Alexandre <amoutonbrady@gmail.com>
2020-04-08 20:04:07 +0200

    Update packages/types/app/vue.d.ts
    Improve type signature + alphabetical order
    
    Co-Authored-By: Kevin Marrec <kevin@marrec.io>

--- packages/types/app/vue.d.ts
+++ packages/types/app/vue.d.ts
@@ -11,8 +11,8 @@ declare module 'vue/types/options' {
   interface ComponentOptions<V extends Vue> {
     asyncData?(ctx: Context): Promise<object | void> | object | void
     fetch?(ctx: Context): Promise<void> | void
-    fetchOnServer?: boolean
     fetchDelay?: number
+    fetchOnServer?: boolean | (() => boolean)
     head?: MetaInfo | (() => MetaInfo)
     key?: string | ((to: Route) => string)
     layout?: string | ((ctx: Context) => string)
