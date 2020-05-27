d1722ea3e22f2ab4d4b26936e559077dd6cb1cc2
Kevin Marrec <kevin@marrec.io>
2020-05-27 13:47:07 +0200

    fix: only accepts POJO values

--- packages/types/config/runtime.d.ts
+++ packages/types/config/runtime.d.ts
@@ -4,8 +4,9 @@
  * NuxtRuntimeConfig interface can be extended by users to enable intellisense on $config
  */
 
-export interface NuxtRuntimeConfig {
-  [key: string]: any
-}
+type JSONObject = { [key: string]: JSONValue }
+type JSONValue = null | undefined | boolean | string | number | JSONObject | JSONValue[]
+
+export interface NuxtRuntimeConfig extends JSONObject {}
 
 export type NuxtConfigurationRuntimeConfig = NuxtRuntimeConfig | ((env: typeof process.env) => NuxtRuntimeConfig)
