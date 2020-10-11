d3b07660e23a2a14d0f71ae211df8cc1a43444c2
Alex Chiu <alexchiu11@gmail.com>
2020-10-11 15:50:02 +0100

    Basic counter example

---
+++ examples/options-api/basic/pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+     <button @click="increment">Increment</button>
+    <p>Counter : {{ counter }}</p>
+    <button @click="decrement">Decrement</button>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increment() {
+      return this.counter++;
+    },
+    decrement() {
+      return this.counter--;
+    }
+  },
+});
+</script>
+
+<style>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height:100vh;
+}
+</style>
