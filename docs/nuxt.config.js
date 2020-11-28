bf8b183e11fa87f0b08e1bcdca9b6cb02c612162
Daniel Roe <daniel@roe.dev>
2020-11-28 20:24:32 +0000

    fix: use native docs colour option

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -3,6 +3,9 @@ import path from 'path'
 import theme from '@nuxt/content-theme-docs'
 
 export default theme({
+  docs: {
+    primaryColor: '#007ACC'
+  },
   css: [path.resolve(__dirname, './assets/custom.css')],
   plugins: [path.resolve(__dirname, './plugins/vue-tabs.js')],
   generate: {
