c6a3b212fb98d080c8ffae8ed4f18f2ca0eb38a0
Daniel Roe <daniel@roe.dev>
2020-11-28 20:49:18 +0000

    fix: add missing pt translation and generate all locales

--- docs/nuxt.config.js
+++ docs/nuxt.config.js
@@ -9,17 +9,32 @@ export default theme({
   css: [path.resolve(__dirname, './assets/custom.css')],
   plugins: [path.resolve(__dirname, './plugins/vue-tabs.js')],
   generate: {
-    routes: ['/']
+    routes: ['/', '/es', 'ja', 'pt', 'zh-Hant', 'zh-Hans']
   },
   i18n: {
     locales: () => [
-      { iso: 'en-US', code: 'en', name: 'English' },
-      { iso: 'ja-JP', code: 'ja', name: '日本語' },
-      { iso: 'es-ES', code: 'es', name: 'Español' },
+      { iso: 'en-US', code: 'en', name: 'English', file: 'en-US.js' },
+      { iso: 'ja-JP', code: 'ja', name: '日本語', file: 'ja_JP.js' },
+      { iso: 'es-ES', code: 'es', name: 'Español', file: 'es-ES.js' },
       { iso: 'pt', code: 'pt', name: 'Portuguese' },
-      { iso: 'zh-Hant', code: 'zh-Hant', name: '繁體中文' },
-      { iso: 'zh-Hans', code: 'zh-Hans', name: '简体中文' }
+      { iso: 'zh-Hant', code: 'zh-Hant', name: '繁體中文', file: 'zh-CN.js' },
+      { iso: 'zh-Hans', code: 'zh-Hans', name: '简体中文', file: 'zh-CN.js' }
     ],
-    defaultLocale: 'en'
+    defaultLocale: 'en',
+    vueI18n: {
+      messages: {
+        pt: {
+          search: {
+            placeholder: 'Pesquisar os docs (Pressione "/" para o foco)'
+          },
+          toc: {
+            title: 'Nesta página'
+          },
+          article: {
+            github: 'Edite esta página no GitHub'
+          }
+        }
+      }
+    }
   }
 })
