150ea058ffa664bdf88dd519c064ae6be8e50a15
Seu Nome <seu_email@example.com>
2025-07-02 16:31:52 -0300

    fix(typescript-build): resolve aliases in dynamic imports

--- packages/typescript-build/src/index.ts
+++ packages/typescript-build/src/index.ts
@@ -51,12 +51,80 @@ const tsModule: Module<Options> = function (moduleOptions) {
   this.extendBuild((config, { isClient, isModern }) => {
     config.resolve!.extensions!.push('.ts', '.tsx')
 
+    // Add alias for @babel/runtime/helpers
+    config.resolve!.alias = {
+      ...config.resolve!.alias,
+      '@babel/runtime/helpers': path.resolve(this.options.rootDir!, 'node_modules/@babel/runtime/helpers')
+    }
+
     const jsxRuleLoaders = config.module!.rules.find(r => (r.test as RegExp).test('.jsx'))!.use as RuleSetUseItem[]
     const babelLoader = jsxRuleLoaders[jsxRuleLoaders.length - 1]
 
     config.module!.rules.push(...(['ts', 'tsx'] as const).map(ext => ({
-      test: new RegExp(`\\.${ext}$`),
-      use: [
+      test: new RegExp(`\.${ext}import path from 'path'
+import { defu } from 'defu'
+import consola from 'consola'
+import type { Module } from '@nuxt/types'
+import type { Options as TsLoaderOptions } from 'ts-loader'
+import type { ForkTsCheckerWebpackPluginOptions as TsCheckerOptions } from 'fork-ts-checker-webpack-plugin/lib/ForkTsCheckerWebpackPluginOptions'
+import type TsCheckerLogger from 'fork-ts-checker-webpack-plugin/lib/logger/Logger'
+import type { RuleSetUseItem } from 'webpack'
+import { NormalModuleReplacementPlugin } from 'webpack'
+
+export interface Options {
+  ignoreNotFoundWarnings?: boolean
+  loaders?: {
+    ts?: Partial<TsLoaderOptions>
+    tsx?: Partial<TsLoaderOptions>
+  }
+  typeCheck?: TsCheckerOptions | boolean
+}
+
+declare module '@nuxt/types' {
+  interface NuxtOptions {
+    typescript: Options
+  }
+}
+
+const defaults: Options = {
+  ignoreNotFoundWarnings: false,
+  typeCheck: true
+}
+
+const tsModule: Module<Options> = function (moduleOptions) {
+  // Combine options
+  const options = defu(this.options.typescript, moduleOptions, defaults)
+
+  // Change color of CLI banner
+  this.options.cli.bannerColor = 'blue'
+
+  if (!this.options.extensions.includes('ts')) {
+    this.options.extensions.push('ts')
+  }
+
+  // Extend Builder to handle .ts/.tsx files as routes and watch them
+  this.options.build.additionalExtensions = ['ts', 'tsx']
+
+  if (options.ignoreNotFoundWarnings) {
+    this.options.build.warningIgnoreFilters!.push(warn =>
+      warn.name === 'ModuleDependencyWarning' && /export .* was not found in /.test(warn.message)
+    )
+  }
+
+  this.extendBuild((config, { isClient, isModern }) => {
+    config.resolve!.extensions!.push('.ts', '.tsx')
+
+    // Add alias for @babel/runtime/helpers
+    config.resolve!.alias = {
+      ...config.resolve!.alias,
+      '@babel/runtime/helpers': path.resolve(this.options.rootDir!, 'node_modules/@babel/runtime/helpers')
+    }
+
+    const jsxRuleLoaders = config.module!.rules.find(r => (r.test as RegExp).test('.jsx'))!.use as RuleSetUseItem[]
+    const babelLoader = jsxRuleLoaders[jsxRuleLoaders.length - 1]
+
+    ),
+      ),      use: [
         babelLoader,
         {
           loader: 'ts-loader',
@@ -83,29 +151,3 @@ const tsModule: Module<Options> = function (moduleOptions) {
         }
       ))
     }
-    if (options.typeCheck && isClient && !isModern) {
-      // eslint-disable-next-line @typescript-eslint/no-var-requires
-      const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
-      const logger = consola.withTag('nuxt:typescript')
-      /* istanbul ignore next */
-      const loggerInterface: TsCheckerLogger = {
-        log (message) { logger.log(message) },
-        info (message) { logger.info(message) },
-        error (message) { logger.error(message) }
-      }
-      config.plugins!.push(new ForkTsCheckerWebpackPlugin(defu(options.typeCheck, {
-        typescript: {
-          configFile: path.resolve(this.options.rootDir!, 'tsconfig.json'),
-          extensions: {
-            vue: true
-          }
-        },
-        logger: {
-          issues: loggerInterface
-        }
-      } as TsCheckerOptions)))
-    }
-  })
-}
-
-export default tsModule
